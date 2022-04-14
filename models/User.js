021050b1530648ea9933c9778779dd4b40abf244
Hemant <hemantrastogi17@gmail.com>
2022-04-14 16:13:43 +0530

    Student/Teacher roles created

--- models/User.js
+++ models/User.js
@@ -2,16 +2,20 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  name:  {
+    name:  {
       type: String,
       required: true
-  }, 
-  email: {
+    }, 
+    email: {
       type: String,
       required: true,
       unique: true
     },
     password: {
+        type: String,
+        required: true
+    },
+    role:{
         type: String,
         required: true
     },
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,51 @@ const jwt = require('jsonwebtoken')
 const JWT_SECRET = "HemantakaFirefist"
 var fetchUser = require('../middleware/fetchUser') 
 
+
+router.post('/teacher',[
+  body('name','Name must contain atleast 3 characters').isLength({ min: 3 }),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password must be contain 8 characters').isLength({ min: 8 })  
+],async(req,res)=>{
+  let ipV4 = req.connection.remoteAddress.replace(/^.*:/, '');
+  if(( ipV4 === 'localhost' ||  ipV4 === '127.0.0.1')){
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try{
+      const salt = await bcrypt.genSalt(10)
+      const secPass = await bcrypt.hash(req.body.password,salt)
+      let user = await User.findOne({email: req.body.email})
+      if(user){
+        return res.status(400).json({err:"Sorry a user with this email already exists"})
+      }
+      user =await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+        role: 'teacher'
+      })
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const jwtToken = jwt.sign(data,JWT_SECRET)
+      res.json({jwtToken})
+      
+    }
+    catch(err){
+      console.log(err.message)
+      res.status(500).send("Some error occured!")
+    }
+  }
+  else{
+    res.status(403).json({err: "Forbidden"})
+  }
+  
+})
+
 // Create a user using: POST "api/auth/createuser" : Doesn;t rqequire authentication
 router.post('/signup',[
     body('name','Name must contain atleast 3 characters').isLength({ min: 3 }),
@@ -29,6 +74,7 @@ router.post('/signup',[
         name: req.body.name,
         email: req.body.email,
         password: secPass,
+        role: 'student'
       })
       const data = {
         user: {
@@ -88,7 +134,7 @@ router.post('/login',[
 
 
 // Get loggedin user details: POST /api/auth/fetchuser. Login required 
-router.post('/fetchUser',fetchUser,async(req, res)=>{
+router.get('/fetchUser',fetchUser,async(req, res)=>{
   try {
     userId = req.user.id
     const user = await User.findById(userId).select("-password")
